c1c224bdec39d4548dbb527c1fcd78979eca763c
rory619 <g00422319@atu.ie>
2024-10-08 11:03:34 +0100

    Committing new changes

---
+++ src/main/java/ie/atu/week3cicd/Week3cicdApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week3cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week3cicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week3cicdApplication.class, args);
+	}
+
+}
